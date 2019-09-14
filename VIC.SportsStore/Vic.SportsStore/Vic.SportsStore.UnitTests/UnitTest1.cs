3d9b67dfc381c82e30e354dd0ee6bf2c9c83677b
Marie <Marie>
2019-09-14 17:35:40 -0400

    1[CHG]Add unit test for list

--- VIC.SportsStore/Vic.SportsStore/Vic.SportsStore.UnitTests/UnitTest1.cs
+++ VIC.SportsStore/Vic.SportsStore/Vic.SportsStore.UnitTests/UnitTest1.cs
@@ -6,6 +6,7 @@ using Moq;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Entities;
 using Vic.SportsStore.WebApp.Controllers;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.UnitTests
 {
@@ -18,17 +19,18 @@ namespace Vic.SportsStore.UnitTests
             // Arrange
             Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
             mock.Setup(m => m.Products).Returns(new Product[] {
-new Product {ProductId = 1, Name = "P1"},
-new Product {ProductId = 2, Name = "P2"},
-new Product {ProductId = 3, Name = "P3"},
-new Product {ProductId = 4, Name = "P4"},
-new Product {ProductId = 5, Name = "P5"}
+new Product {ProductId = 1, Name = "P1",Category="Fruit"},
+new Product {ProductId = 2, Name = "P2",Category="Fruit"},
+new Product {ProductId = 3, Name = "P3",Category="Fruit"},
+new Product {ProductId = 4, Name = "P4",Category="Fruit"},
+new Product {ProductId = 5, Name = "P5",Category="Fruit"}
 });
             ProductController controller = new ProductController();
             controller.ProductsRepository = mock.Object;
             controller.PageSize = 3;
             // Act
-            var result = (IEnumerable<Product>)controller.List("Fruit",2).Model;
+            var resultModel = (ProductsListViewModel)controller.List("Fruit",1).Model;
+            var result = resultModel.Products;
             // Assert
             Product[] prodArray = result.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
